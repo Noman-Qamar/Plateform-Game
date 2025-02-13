3ff30200872bb38397a951ef92ee65a2260985eb
Noman-Qamar <noman.qamar@gmail.com>
2025-02-13 12:03:02 +0500

    progressing

--- start.js
+++ start.js
@@ -62,5 +62,50 @@ var State = class State {
         let actors = this.actors.map (actor => actor.update(time, this, keys));
         let newState = new State (this.level, actors, this.status);
         if (newState.status != "playing") return newState;
+        let player = newState.player;
+        if (this.level.touches(player.pos, player.size, "lava")) {
+            return new State (this.level, actors, "lost");            
+        }
+        for (let actor of actors) {
+            if (actor != player && overlap (actor, player)) {
+                newState = actor.collide(newState);
+            }
+        }
+        return newState;
+    };
+}
+
+var Vec = class Vec {
+    constructor (x, y) {
+        this.x = x; this.y = y;
     }
+    plus(other) {
+        return new Vec (this.x + other.x, this.y + other.y);
+    }
+    times(factor) {
+        return new Vec (this.x * factor, this.y * factor);
+    }
+}
+
+var Player = class Player {
+    constructor (pos, speed) {
+        this.pos = pos;
+        this.speed = speed;
+    }
+    get type () {return "player";}
+    get size () {return new Vec(0.8, 1.5);}
+
+    static create (pos) {
+        return new Player(pos.plus(new Vec(0, -0.5)), new Vec(0,0));
+    }
+
+    update(time, state, keys) {
+        
+    }
+}
+function overlap(actor1, actor2) {
+    return  actor1.pos.x < actor2.pos.x + actor2.size.x &&
+            actor1.pos.x + actor1.size.x > actor2.pos.x &&
+            actor1.pos.y < actor2.pos.y + actor2.size.y &&
+            actor1.pos.y + actor1.size.y > actor2.pos.y    
 }
