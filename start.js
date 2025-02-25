a4b0369ab08fbf19998355e82b10ba6b1af5d157
Noman-Qamar <noman.qamar@gmail.com>
2025-02-25 09:32:00 +0500

    progressing

--- start.js
+++ start.js
@@ -207,7 +207,14 @@ function drawGrid(level) {
     ))
 }
 function drawActors (actors) {
-    return elt
+    return elt("div", {}, ...actors.map(actor => {
+        let rect = elt("div", {class: `actor ${actor.typ}`});
+        rect.style.width = `${actor.size.x * scale}px`;
+        rect.style.height = `${actor.size.y * scale}px`;
+        rect.style.left = `${actor.pos.x * scale}px`;
+        rect.style.top = `${actor.pos.y * scale}px`;
+        return rect;
+    }))
 }
 function overlap(actor1, actor2) {
     return  actor1.pos.x < actor2.pos.x + actor2.size.x &&
